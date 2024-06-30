8289218fbbc47753e4280105ae21c017c3e0a187
Aaron Pierce <aaron@cute.engineer>
2024-06-30 12:53:52 +1000

    build(nix): adding flake, direnv setup

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "environment for eslint-plugin-nodash";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem
+    (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.nodejs_20 ];
+        };
+      }
+    );
+}
