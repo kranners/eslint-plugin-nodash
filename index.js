6c53e1fe0881b216b13a9ed46235e086c788692a
Aaron Pierce <aaron@cute.engineer>
2024-07-11 21:35:00 +1000

    build: changing to a module

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-import { readFileSync } from "fs";
-import { join } from "path";
-
-const { version } = JSON.parse(
-  readFileSync(join(__dirname, "package.json"))
-);
-
 const plugin = {
   meta: {
     name: "eslint-plugin-nodash",
-    version,
   },
   rules: {},
 };
