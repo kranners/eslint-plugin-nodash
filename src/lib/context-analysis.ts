2150797129f025c508f83efb93c9011987a8364b
Aaron Pierce <aaron@cute.engineer>
2024-08-17 16:46:39 +1000

    feat(lib): adding methods for checking imports

---
+++ src/lib/context-analysis.ts
@@ -0,0 +1,68 @@
+import { isIdentifier } from "@typescript-eslint/utils/ast-utils";
+import { SourceCode } from "@typescript-eslint/utils/ts-eslint";
+import { Identifier, ImportStatement, isImportDeclaration, isImportStatement, isLiteral, isMemberExpression, isObjectPattern, MemberExpression } from "lib/type-guards";
+
+const isImportStatementLodash = (statement: ImportStatement): boolean => {
+  if (isImportDeclaration(statement)) {
+    return statement.source.value === "lodash";
+  }
+
+  const call = statement.declarations[0].init;
+  if (call.arguments.length !== 1) {
+    return false;
+  }
+
+  const [required] = call.arguments;
+  if (!isLiteral(required)) {
+    return false;
+  }
+
+  return required.value === "lodash";
+}
+
+const getMemberExpressionAncestor = (expression: MemberExpression): Identifier | undefined => {
+  if (isIdentifier(expression.object)) {
+    return expression.object;
+  }
+
+  if (isMemberExpression(expression.object)) {
+    return getMemberExpressionAncestor(expression.object);
+  }
+
+  return undefined;
+}
+
+const isNodeFromImportStatement = (statement: ImportStatement, node: Identifier | MemberExpression): boolean => {
+  const target: Identifier | undefined = isIdentifier(node) ? node : getMemberExpressionAncestor(node);
+
+  if (target === undefined) {
+    return false;
+  }
+
+  if (isImportDeclaration(statement)) {
+    return statement.specifiers.some((specifier) => {
+      return specifier.local.name === target.name;
+    });
+  }
+
+  const [declaration] = statement.declarations;
+  if (isIdentifier(declaration.id)) {
+    return declaration.id.name === target.name;
+  }
+
+  if (isObjectPattern(declaration.id)){
+    return declaration.id.properties.some((property) => {
+      if (isIdentifier(property.value)) {
+        return property.value.name === target.name;
+      }
+    })
+  }
+
+  return false;
+}
+
+export const isNodeFromLodash = (ast: SourceCode.Program, node: Identifier | MemberExpression): boolean => {
+  const imports = ast.body.filter(isImportStatement).filter(isImportStatementLodash);
+
+  return imports.some((statement) => isNodeFromImportStatement(statement, node));
+}
--- src/lib/type-guards.ts
+++ src/lib/type-guards.ts
@@ -1,4 +1,5 @@
 import { AST_NODE_TYPES, ASTUtils, TSESTree } from "@typescript-eslint/utils";
+import { isIdentifier } from "@typescript-eslint/utils/ast-utils";
 
 export type Expression = TSESTree.Expression;
 export type Identifier = TSESTree.Identifier;
@@ -9,14 +10,56 @@ export type Literal = TSESTree.Literal;
 export type LogicalExpression = TSESTree.LogicalExpression;
 
 export const isLogicalExpression = ASTUtils.isNodeOfType(AST_NODE_TYPES.LogicalExpression);
-
 export const isMemberExpression = ASTUtils.isNodeOfType(AST_NODE_TYPES.MemberExpression);
-
 export const isSpreadElement = ASTUtils.isNodeOfType(AST_NODE_TYPES.SpreadElement);
-
 export const isLiteral = ASTUtils.isNodeOfType(AST_NODE_TYPES.Literal);
+export const isImportDeclaration = ASTUtils.isNodeOfType(AST_NODE_TYPES.ImportDeclaration);
+export const isVariableDeclaration = ASTUtils.isNodeOfType(AST_NODE_TYPES.VariableDeclaration);
+export const isCallExpression = ASTUtils.isNodeOfType(AST_NODE_TYPES.CallExpression);
+export const isImportDefaultSpecifier = ASTUtils.isNodeOfType(AST_NODE_TYPES.ImportDefaultSpecifier);
+export const isImportSpecifier = ASTUtils.isNodeOfType(AST_NODE_TYPES.ImportSpecifier);
+export const isObjectPattern = ASTUtils.isNodeOfType(AST_NODE_TYPES.ObjectPattern);
 
 export const isValidCallee = (callee: Expression): callee is Identifier | MemberExpression => {
   return ASTUtils.isIdentifier(callee) || isMemberExpression(callee);
 }
 
+export type CommonJSImport = TSESTree.VariableDeclaration & {
+  declarations: [
+    TSESTree.VariableDeclarator & {
+      init: TSESTree.CallExpression & {
+        callee: TSESTree.Identifier & {
+          name: "require"
+        }
+      }
+    }
+  ]
+};
+
+export type ImportStatement = TSESTree.ImportDeclaration | CommonJSImport;
+
+export const isImportStatement = (statement: TSESTree.ProgramStatement): statement is ImportStatement => {
+  if (isImportDeclaration(statement)) {
+    return true;
+  }
+
+  if (!isVariableDeclaration(statement)) {
+    return false;
+  }
+
+  if (statement.declarations.length !== 1) {
+    return false;
+  }
+
+  const [declaration] = statement.declarations;
+
+  if (!isCallExpression(declaration.init)) {
+    return false;
+  }
+
+  if (!isIdentifier(declaration.init.callee)) {
+    return false;
+  }
+
+  return declaration.init.callee.name === "require";
+}
